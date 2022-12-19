d39742e2b742181fdcd0de788a18ecbc4400bc58
Dlima15 <danilo.lima7456@gmail.com>
2022-12-19 09:51:10 -0300

    modal funcional javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+function MyFunction(){
+    var x = document.getElementById("MyDialog");
+    x.open = true;
+}
+
+function close(){
+    var x = document.getElementById("MyDialog");
+    x.close = 0;
+}
+
+
